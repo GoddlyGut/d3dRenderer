9e4997d4b99bcbcf481f5e358a7c6f24a7e581ea
GoddlyGut <100250431+GoddlyGut@users.noreply.github.com>
2023-11-02 22:08:48 -0400

    cleaned the code up

--- d3dRenderer/shaders.hlsl
+++ d3dRenderer/shaders.hlsl
@@ -1,4 +1,10 @@
-struct PSInput
+struct VS_INPUT
+{
+    float4 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct VS_OUTPUT
 {
     float4 position : SV_POSITION;
     float4 color : COLOR;
@@ -12,22 +18,22 @@ cbuffer MVPMatrixBuffer : register(b0)
 }
 
 
-PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
+VS_OUTPUT VSMain(VS_INPUT input)
 {
-    PSInput result;
+    VS_OUTPUT result;
 
     // Apply the model, view, and projection matrices to transform the vertex position
-     float4 worldPosition = mul(position, model);
+     float4 worldPosition = mul(input.position, model);
      float4 viewPosition = mul(worldPosition, view);
      result.position = mul(viewPosition, projection);
 
     //result.position = position;
-    result.color = color;
+    result.color = input.color;
 
     return result;
 }
 
-float4 PSMain(PSInput input) : SV_TARGET
+float4 PSMain(VS_OUTPUT input) : SV_TARGET
 {
     return input.color;
 }
